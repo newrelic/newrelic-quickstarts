9bac2bcc219269e94ee11bab81fdd0fa121e0c60
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-05-07 14:28:10 +0200

    Added types for dashboard input

---
+++ .github/actions/import-data/types/dashboardInput.ts
@@ -0,0 +1,94 @@
+export type dashboardBody = {
+    description: string,
+    name: string,
+    pages: dashboardPageInput[],
+    permissions: string,
+}
+
+type dashboardPageInput = {
+    description: string,
+    guid?: unknown, 
+    name: string,
+    widgets: dashboardWidgetInput[],
+}
+
+type dashboardWidgetInput = {
+    id?: string,
+    title?: string,
+    configuration?: dashboardWidgetConfigurationInput,
+    layout?: dashboardWidgetLayoutInput,
+    linkedEntityGuids: unknown[],
+    rawConfiguration?: unknown,
+    visualization?: dashboardWidgetVisualizationInput,
+}
+
+type dashboardWidgetConfigurationInput = {
+    area?: dashboardBasicWidgetConfigurationInput,
+    bar?: dashboardBasicWidgetConfigurationInput,
+    billboard?: dashboardBillboardWidgetConfigurationInput,
+    line?: dashboardBasicWidgetConfigurationInput,
+    markdown?: dashboardMarkdownWidgetConfigurationInput,
+    pie?: dashboardBasicWidgetConfigurationInput,
+    table?: dashboardBasicWidgetConfigurationInput,
+}
+
+type dashboardBasicWidgetConfigurationInput = {
+    nrqlQueries: dashboardWidgetNrqlQueryInput[],
+}
+
+// type dashboardAreaWidgetConfigurationInput = {
+//     nrqlQueries: dashboardWidgetNrqlQueryInput[],
+// }
+
+// type dashboardBarWidgetConfigurationInput = {
+//     nrqlQueries: dashboardWidgetNrqlQueryInput[],
+// }
+
+// type dashboardLineWidgetConfigurationInput = {
+//     nrqlQueries: dashboardWidgetNrqlQueryInput[],
+// }
+
+// type dashboardPieWidgetConfigurationInput = {
+//     nrqlQueries: dashboardWidgetNrqlQueryInput[],
+// }
+
+// type dashboardTableWidgetConfigurationInput = {
+//     nrqlQueries: dashboardWidgetNrqlQueryInput[],
+// }
+
+type dashboardMarkdownWidgetConfigurationInput = {
+    text: string,
+}
+
+type dashboardBillboardWidgetConfigurationInput = {
+    nrqlQueriees: dashboardWidgetNrqlQueryInput[],
+    thresholds: dashboardBillboardWidgetThresholdInput[],
+}
+
+type dashboardBillboardWidgetThresholdInput = {
+    value?: number,
+    alertSeverity?: severity,
+}
+
+enum severity {
+    'CRITICAL',
+    'NOT_ALERTING',
+    'WARNING',
+}
+
+type dashboardWidgetNrqlQueryInput = {
+    accountId: number,
+    query: string,
+}
+
+
+type dashboardWidgetLayoutInput = {
+    column?: number,
+    height?: number,
+    row?: number,
+    widht?: number,
+}
+
+type dashboardWidgetVisualizationInput = {
+    id?: string,
+}
