48f5f7b27bfefb462733251d0821f95e592e38e4
Jakub Kotkowiak <cckotkowiak@gmail.com>
2021-05-19 13:32:33 +0200

    Add error handling

--- .github/actions/import-data/src/importer.ts
+++ .github/actions/import-data/src/importer.ts
@@ -1,5 +1,4 @@
 import { dashboardBody, importedDashboardBody } from './types/dashboardInput';
-import { AlertsUnion } from './types/alertsNrqlCondition';
 import { GraphQLClient } from 'graphql-request';
 import addDashboard from './mutations/dashboard';
 import addPolicy from './mutations/policy';
@@ -9,154 +8,164 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
-
 const url = 'https://api.newrelic.com/graphql';
 
 const client = new GraphQLClient(url, {
-    headers: {'Content-Type': 'application/json'}
+	headers: { 'Content-Type': 'application/json' },
 });
 
-export const importer = async (
-  accountId: number,
-  nrApiKey: string,
-  dashboardPack: string
-): Promise<void> => {
-  let usingActions = true;
-  if (!accountId && !nrApiKey && !dashboardPack) {
-    const args = yargs.options({
-      accountId: {
-        alias: 'id',
-        demandOption: true,
-        type: 'number',
-        description: 'NR account id'
-      },
-      nrApiKey: {
-        alias: 'key',
-        demandOption: true,
-        type: 'string',
-        description: 'NR API Key'
-      },
-      dashboardPack: {
-        alias: 'pack',
-        demandOption: true,
-        type: 'string',
-        description: 'NR API Key'
-      }
-    }).argv
+export const importer = async (accountId: number, nrApiKey: string, dashboardPack: string): Promise<void> => {
+	if (!accountId && !nrApiKey && !dashboardPack) {
+		const args = yargs.options({
+			accountId: {
+				alias: 'id',
+				demandOption: true,
+				type: 'number',
+				description: 'NR account id',
+			},
+			nrApiKey: {
+				alias: 'key',
+				demandOption: true,
+				type: 'string',
+				description: 'NR API Key',
+			},
+			dashboardPack: {
+				alias: 'pack',
+				demandOption: true,
+				type: 'string',
+				description: 'NR API Key',
+			},
+		}).argv;
 
-    accountId = args.accountId
-    nrApiKey = args.nrApiKey
-    dashboardPack = args.dashboardPack
+		accountId = args.accountId;
+		nrApiKey = args.nrApiKey;
+		dashboardPack = args.dashboardPack;
+	}
+	client.setHeader('API-Key', nrApiKey);
 
-    usingActions = false;
-  };
-  client.setHeader('API-Key', nrApiKey);
+	dashboardPack = dashboardPack.toLowerCase();
+	const policyId = await createPolicy(accountId, dashboardPack);
 
-  dashboardPack = dashboardPack.toLowerCase();
-  const policyId = await createPolicy(accountId, dashboardPack);
-
-
-  if(!usingActions){
-    await createDashboardLocal(accountId, dashboardPack);
-
-    await createAlertLocal(accountId, dashboardPack, policyId);
-  }
-  
-}
+	await createDashboardLocal(accountId, dashboardPack);
+	await createAlertLocal(accountId, dashboardPack, policyId);
+};
 
 const createPolicy = async (accountId: number, pack: string) => {
-  const policyName = pack.charAt(0).toUpperCase() + pack.slice(1) + ' default alert policy';
-  
-  const variables = {
-    accountId: accountId,
-    name: policyName
-  }
-  const response = await client.request(addPolicy, variables);
-  return response.alertsPolicyCreate.id;
-}
+	const policyName = `${pack.charAt(0).toUpperCase() + pack.slice(1)} default alert policy`;
+
+	const variables = {
+		accountId,
+		name: policyName,
+	};
+	const response = await client.request(addPolicy, variables);
+	return response.alertsPolicyCreate.id;
+};
 
 const createDashboardLocal = async (accountId: number, pack: string) => {
-  const replacer = new RegExp('"accountId":0', 'g');
+	const replacer = new RegExp('"accountId":0', 'g');
 
-  const dir = `${__dirname}/../../../../${pack}/dashboards`;
-  const importedFiles: Array<importedDashboardBody> = fs
-    .readdirSync(dir)
-    .filter(name => path.extname(name) === '.json')
-    .map(name => require(path.join(dir, name)));
+	const dir = `${__dirname}/../../../../${pack}/dashboards`;
+	let importedFiles: importedDashboardBody[] = [];
 
-  importedFiles.forEach(file => {
-    file.permissions = 'PUBLIC_READ_WRITE';
-    let stringifiedDashboard = JSON.stringify(file);
-    stringifiedDashboard = stringifiedDashboard.replace(
-      replacer,
-      `"accountId": ${accountId}`
-    );
+	try {
+		importedFiles = fs
+			.readdirSync(dir)
+			.filter(name => path.extname(name) === '.json')
+			.map(name => require(path.join(dir, name)));
+	} catch (error) {
+		console.error('Dashboard files not found. Did you provide the correct pack name?');
+	}
 
-    const parsedDashboard: dashboardBody = JSON.parse(stringifiedDashboard);
+	importedFiles.forEach(async file => {
+		file.permissions = 'PUBLIC_READ_WRITE';
+		let stringifiedDashboard = JSON.stringify(file);
+		stringifiedDashboard = stringifiedDashboard.replace(replacer, `"accountId": ${accountId}`);
 
-    const variables = {
-      accountId: accountId,
-      dashboard: parsedDashboard
-    };
+		const parsedDashboard: dashboardBody = JSON.parse(stringifiedDashboard);
 
-    client.request(addDashboard, variables);
-  })
-}
+		const variables = {
+			accountId,
+			dashboard: parsedDashboard,
+		};
+		try {
+			await client.request(addDashboard, variables);
+		} catch (error) {
+			console.error('Dashboard Failure: ', error.response.errors[0].message);
+		}
+	});
+};
 
 const createAlertLocal = async (accountId: number, pack: string, policyId: number) => {
-  let variables = {
-    accountId: accountId,
-    condition: undefined,
-    policyId: policyId
-  };
+	const variables = {
+		accountId,
+		condition: undefined,
+		policyId,
+	};
 
-  const dir = `${__dirname}/../../../../${pack}/alerts`
+	const dir = `${__dirname}/../../../../${pack}/alerts`;
+	let fileNames: string[] = [];
 
-  const fileNames: Array<string> = fs
-    .readdirSync(dir)
-    .filter(name => path.extname(name) === '.yml');
+	try {
+		fileNames = fs.readdirSync(dir).filter(name => path.extname(name) === '.yml');
+	} catch (error) {
+		console.error('Alert files not found. Did you provide the correct pack name?');
+	}
 
-  fileNames.forEach(async file => {
-    const loadedYaml = yaml.load(fs.readFileSync(`${dir}/${file}`, 'utf-8'));
-    let parsedAlert = JSON.parse(JSON.stringify(loadedYaml));
+	fileNames.forEach(async file => {
+		const loadedYaml = yaml.load(fs.readFileSync(`${dir}/${file}`, 'utf-8'));
+		const parsedAlert = JSON.parse(JSON.stringify(loadedYaml));
 
-    if(parsedAlert.type === 'BASELINE') {
-      let filledFile = transformData(parsedAlert);
-      variables.condition = filledFile;
-      await client.rawRequest(baselineMutation, variables);
-    } else if (parsedAlert.type === 'STATIC') {
-      let filledFile = transformData(parsedAlert);
-      variables.condition = filledFile;
-      await client.rawRequest(staticMutation, variables);
-    } else if (parsedAlert.type === 'OUTLIER') {
-      let filledFile = transformData(parsedAlert);
-      variables.condition = filledFile;
-      await client.rawRequest(outlierMutation, variables);
-    }
-  });
+		if (parsedAlert.type === 'BASELINE') {
+			const filledFile = transformData(parsedAlert);
+			variables.condition = filledFile;
+
+			try {
+				await client.rawRequest(baselineMutation, variables);
+			} catch (error) {
+				console.error('Alert Failure: ', error.response.errors[0].message);
+			}
+		} else if (parsedAlert.type === 'STATIC') {
+			const filledFile = transformData(parsedAlert);
+			variables.condition = filledFile;
+
+			try {
+				await client.rawRequest(staticMutation, variables);
+			} catch (error) {
+				console.error('Alert Failure: ', error.response.errors[0].message);
+			}
+		} else if (parsedAlert.type === 'OUTLIER') {
+			const filledFile = transformData(parsedAlert);
+			variables.condition = filledFile;
+
+			try {
+				await client.rawRequest(outlierMutation, variables);
+			} catch (error) {
+				console.error('Alert Failure: ', error.response.errors[0].message);
+			}
+		}
+	});
 };
 
 const transformData = (incomingFile: any) => {
-  if(!incomingFile.enabled) {
-    incomingFile.enabled = false;
-  }
+	if (!incomingFile.enabled) {
+		incomingFile.enabled = false;
+	}
 
-  if(incomingFile.type === 'BASELINE') {
-    incomingFile.terms.forEach((term: { operator: string; }) => {
-      term.operator = 'ABOVE'
-    });
-  }
+	if (incomingFile.type === 'BASELINE') {
+		incomingFile.terms.forEach((term: { operator: string }) => {
+			if (!term.operator) term.operator = 'ABOVE';
+		});
+	}
 
-  if(incomingFile.type) {
-    delete incomingFile.type
-  }
+	if (incomingFile.type) {
+		delete incomingFile.type;
+	}
 
-  if(incomingFile.details) {
-    delete incomingFile.details
-  }
+	if (incomingFile.details) {
+		delete incomingFile.details;
+	}
 
-  return incomingFile;
-}
+	return incomingFile;
+};
 
-
-export default importer
+export default importer;
