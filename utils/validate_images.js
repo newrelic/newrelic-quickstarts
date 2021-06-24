4b27997956119a33d1aa4b821e580f8e8b1d2cf6
rudouglas <ruairidouglas@gmail.com>
2021-06-24 17:19:51 +0100

    chore: use glob and combine with validate-packs workflow

--- utils/validate_images.js
+++ utils/validate_images.js
@@ -1,8 +1,10 @@
 'use strict';
 const core = require('@actions/core');
-const {promises:fs, statSync} = require("fs");
+const fs = require("fs");
 const path = require('path');
 const isImage = require('is-image');
+const glob = require('glob');
+
 
 const BASE_PATH = './packs/';
 const MAX_SIZE = 4000000;
@@ -20,68 +22,58 @@ let valid = true,
     
 /** 
 * Validates an object against a JSON schema
-* @param {String} dir - The directory to parse, set by the command line argument
-* @returns {Object[]} An array of any errors found
+* @param {String} dir - The directory to parse, set by the BASE_PATH variable
 */
-async function getFiles(dir) {
-  
-  const entries = await fs.readdir(dir, { withFileTypes: true });
 
+const globFiles = (dir) => {
+  const entries = [...glob.sync(path.resolve(dir, '**/*'))]
   // Get files within the current directory and add a path key to the file objects
   const files = entries
-      .filter(file => !file.isDirectory())
-      .filter(file => isImage(dir + file.name))
+      .filter(file => isImage(file))
       .map(file => {
-        // console.log(file.name)
-        const filePath = dir + file.name
-        const fileExt = path.extname(filePath);
-        const fileSize = statSync(filePath)['size']
-        if (fileSize > MAX_SIZE){
-            sizeErrors.push({[filePath]: `${fileSize/1000000}MB`})
-            valid = false
-        }
+        const fileExt = path.extname(file);
         if (!ALLOWED_IMG_EXT.includes(fileExt)){
-            typeErrors.push(filePath)
+          typeErrors = [...typeErrors, file]
+          valid = false
+        }
+
+        const fileSize = fs.statSync(file)['size']
+        if (fileSize > MAX_SIZE){
+            sizeErrors = [...sizeErrors, {[file]: `${fileSize/1000000}MB`}]
             valid = false
         }
       })
-
-  if (files.length > MAX_NUM_IMG) {
-      tooManyImages.push({[dir]: files.length});
-      valid = false;
-  }
-  // Get folders within the current directory
-  const folders = entries.filter(folder => folder.isDirectory());
   
-  for (const folder of folders)
-      /*
-        Add the found files within the subdirectory to the files array by calling the
-        current function itself
-      */
-    
-      files.push(...await getFiles(`${dir}${folder.name}/`));
-  return files;
+  const directories = entries
+    .filter(file => fs.statSync(file).isDirectory())
+    .map(folder => {
+      const imageCount = [...glob.sync(path.resolve(folder, '**/*'))]
+        .filter(file => isImage(file)).length
+      if (imageCount > MAX_NUM_IMG) {
+          tooManyImages = [...tooManyImages, {[folder]: imageCount}]
+          valid = false;
+      }
+    })
 }
 
 const main = () => {
- getFiles(BASE_PATH).then(() => {
-    if(!valid) {
-        if (typeErrors.length > 0) {
-          console.warn(`\nImages should be of format ${[...ALLOWED_IMG_EXT]}:`)
-          typeErrors.map((file) => console.warn(file))
-        }
-        if (sizeErrors.length > 0) {
-          console.warn(`\nImages should be below ${MAX_SIZE/1000000}MB:`)
-          sizeErrors.map((file) => console.warn(file))
-        }
-        if (tooManyImages.length > 0) {
-          console.warn(`\nComponents should contain less than 6 images:`)
-          tooManyImages.map((dir) => console.warn(dir))
-        }
-        core.setFailed('Check image requirements!')
-    }
-  }).catch(e => console.log(e))
-  
+  globFiles(BASE_PATH)
+
+  if(!valid) {
+      if (typeErrors.length > 0) {
+        console.warn(`\nImages should be of format ${[...ALLOWED_IMG_EXT]}:`)
+        typeErrors.map((file) => console.warn(file))
+      }
+      if (sizeErrors.length > 0) {
+        console.warn(`\nImages should be below ${MAX_SIZE/1000000}MB:`)
+        sizeErrors.map((file) => console.warn(file))
+      }
+      if (tooManyImages.length > 0) {
+        console.warn(`\nComponents should contain less than 6 images:`)
+        tooManyImages.map((dir) => console.warn(dir))
+      }
+      core.setFailed('Check image requirements!')
+  }  
 }
 
 main();
