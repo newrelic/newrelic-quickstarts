d437c185bfa7777efbaaa3d9d6231a6e279c8277
lbaker <lbaker@newrelic.com>
2021-08-23 09:47:40 -0700

    chore: Update label script

--- utils/check_support_level.js
+++ utils/check_support_level.js
@@ -1,8 +1,12 @@
 const path = require('path');
 const fetch = require('node-fetch');
 const parseLinkHeader = require('parse-link-header');
-const glob = require('glob');
-const { readYamlFile, checkArgs } = require('./helpers');
+const {
+  readYamlFile,
+  checkArgs,
+  removeRepoPathPrefix,
+  findMainPackConfigFiles,
+} = require('./helpers');
 
 const fetchFilesFromGH = async (url) => {
   let files = [];
@@ -34,29 +38,38 @@ const getNextLink = (linkHeader) => {
 };
 
 const findSupportLevel = async (url) => {
-  let packAddition = false;
+  let newPack = false;
 
-  const files = await fetchFilesFromGH(url);
-  const packNames = files.reduce((acc, file) => {
+  const files = [
+    { filename: 'packs/python/python/config.yml', status: 'added' },
+    { filename: 'packs/java/java/dashboards/boop.js', status: 'modified' },
+    { filename: 'packs/apache/config.yml', status: 'modified' },
+  ];
+
+  const configFiles = findMainPackConfigFiles();
+  files.forEach((file) => {
     if (file.filename.includes('config.yml') && file.status === 'added') {
-      packAddition = true;
+      newPack = true;
     }
-    if (file.filename.includes('packs/')) {
-      acc.push(file.filename.replace('packs/', '').split('/')[0]);
-    }
-    return acc;
-  }, []);
+  });
+
+  const configFilesToRead = configFiles
+    .map((config) => {
+      const configRelativePath = removeRepoPathPrefix(config);
+      const regex = new RegExp(`^${path.dirname(configRelativePath)}.*`);
+      console.log(regex);
+      const fileMatch = files.find((file) => file.filename.match(regex));
+
+      return fileMatch !== undefined && config;
+    })
+    .filter(Boolean);
 
   const supportLevelSet = new Set();
 
-  packNames.forEach((packName) => {
-    const parsedConfig = readYamlFile(
-      glob.sync(
-        path.resolve(process.cwd(), `../packs/${packName}/config.+(yml|yaml)`)
-      )[0]
-    );
+  configFilesToRead.forEach((configFile) => {
+    const parsedConfig = readYamlFile(configFile);
     const supportLevel = parsedConfig.contents[0].level;
-
+    console.log(supportLevel, configFile);
     if (supportLevel) {
       const validSupportLevels = ['New Relic', 'Verified', 'Community'];
       if (validSupportLevels.includes(supportLevel)) {
@@ -67,7 +80,7 @@ const findSupportLevel = async (url) => {
 
   const supportLevelArray = Array.from(supportLevelSet);
 
-  console.log(`::set-output name=addition::${packAddition}`);
+  console.log(`::set-output name=addition::${newPack}`);
 
   if (supportLevelArray.includes('New Relic')) {
     console.log('::set-output name=newrelic::true');
