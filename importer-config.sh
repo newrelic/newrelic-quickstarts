b2d17bc2e789ec77166e4530fddcebd6205fb5c2
Stijn Polfliet <spolfliet@newrelic.com>
2021-09-17 15:52:09 +0200

    fix: remove config

--- importer-config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# How to find your accountId: https://docs.newrelic.com/docs/accounts/accounts-billing/account-setup/account-id/
-accountId=2136679
-# How to create a User API Key: https://docs.newrelic.com/docs/apis/intro-apis/new-relic-api-keys/#user-key-create
-apiKey=793KMSYWHP503FQ4BCDDHC5TXPN3ZQNN
