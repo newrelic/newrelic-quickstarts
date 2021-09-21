f5e5c7ab39ad14872886d1dfc30815482379431d
Josh Biggley <79025418+nr-jbiggley@users.noreply.github.com>
2021-09-21 20:50:42 -0300

    Delete importer-config.sh

--- importer-config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# How to find your accountId: https://docs.newrelic.com/docs/accounts/accounts-billing/account-setup/account-id/
-accountId=2548172
-# How to create a User API Key: https://docs.newrelic.com/docs/apis/intro-apis/new-relic-api-keys/#user-key-create
-apiKey=7df3c6c60b92bf4baf43d005696864b85aa5NRAL
