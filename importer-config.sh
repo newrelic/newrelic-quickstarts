c7204ee98fc02ed3c54551a2537f725f5cad94a8
Daniel Kim <dgkim@ucdavis.edu>
2021-09-22 10:32:25 -0700

    Update importer-config.sh

--- importer-config.sh
+++ importer-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # How to find your accountId: https://docs.newrelic.com/docs/accounts/accounts-billing/account-setup/account-id/
-accountId=3283819
+accountId=
 # How to create a User API Key: https://docs.newrelic.com/docs/apis/intro-apis/new-relic-api-keys/#user-key-create
-apiKey=NRAK-XXXLSSYU5WC3G677N38ZYB99V79
+apiKey=
