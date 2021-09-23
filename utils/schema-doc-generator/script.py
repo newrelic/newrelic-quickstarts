0cbd42242b8269dad5e3a298a64db020a567f713
Kris Kenney <70179215+nr-kkenney@users.noreply.github.com>
2021-09-23 13:09:56 -0700

    chore: update script to reword footer
    * previously, generated footer also included a timestamp, which would
    indicate to git the file was always changing, despite no meaningful
    changes to the schema. Stripped the timestamp off the footer to try
    and avoid this. Now, git should only see & commit changes if the schema
    changes in some meaningful way.

--- utils/schema-doc-generator/script.py
+++ utils/schema-doc-generator/script.py
@@ -1,8 +1,19 @@
+import fileinput
 import glob
 from pathlib import Path
+import sys
 
 from json_schema_for_humans.generation_configuration import GenerationConfiguration
 from json_schema_for_humans.generate import generate_from_filename
+
+def reword_footer(file_path):
+    with fileinput.input(files=(file_path), inplace=True) as f:
+        for line in f:
+            search_string = "Generated using [json-schema-for-humans](https://github.com/coveooss/json-schema-for-humans)"
+            if search_string in line:
+                # this line strips off the timestamp, so the file doesn't all change and get picked up by git
+                line = "Generated using [json-schema-for-humans](https://github.com/coveooss/json-schema-for-humans)"
+            sys.stdout.write(line)
 
 config = GenerationConfiguration(template_name="md", show_toc=False)
 
@@ -13,3 +24,4 @@ for path in schema_paths:
     stem = Path(path).stem
     print(f'generating doc for: {path}')
     generate_from_filename(path, f'docs/{stem}.md', config=config)
+    reword_footer(f'docs/{stem}.md')
