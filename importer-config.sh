1765cfebd2dbee42ba34459075d974e8a10efdee
tanate <tanate@newrelic.com>
2021-10-11 20:40:08 -0300

    feat(log-analysis): Added log-analysis dashboard

--- importer-config.sh
+++ importer-config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # How to find your accountId: https://docs.newrelic.com/docs/accounts/accounts-billing/account-setup/account-id/
-accountId=
+accountId=1606862
 # How to create a User API Key: https://docs.newrelic.com/docs/apis/intro-apis/new-relic-api-keys/#user-key-create
-apiKey=
+apiKey=NRAK-XTAO8ZHF527WHR56AFH115BP7RA
