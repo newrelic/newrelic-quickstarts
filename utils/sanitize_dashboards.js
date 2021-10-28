1d987825546ecb098fc24d70e1f35e4b4d7ed54a
Clark McAdoo <jmcadoo@newrelic.com>
2021-10-28 17:49:04 -0500

    fix: update documentation to be more encompassing, fix potential path errors for windows users, update regex to not remove extra lines or insert unneeded spacing

--- utils/sanitize_dashboards.js
+++ utils/sanitize_dashboards.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { readdir } = require('fs').promises;
+const path = require('path');
 // Removes the first two arguments passed by `node`, these aren't needed for this function to run - CM
 const pathsToSanitize = process.argv.slice(2);
 
@@ -10,14 +11,15 @@ if (pathsToSanitize.length < 1)
 
 const sanitizeDashboard = (fileContent) => {
   return fileContent
-    .replace(/\"accountId\"\s*:\s*\d+\s*/g, '"accountId": 0') // Anonymize account ID
-    .replace(/^.+\"linkedEntityGuids\".+\n?/gm, '"linkedEntityGuids": null') // Remove linkedEntityGuids
+    .replace(/\"accountId\"\s*:\s*\d+/g, '"accountId": 0') // Anonymize account ID
+    .replace(/\"linkedEntityGuids\"\s*:\s*\[.*\]/g, '"linkedEntityGuids": null') // Set linkedEntityGuids to null
     .replace(/^.+\"permissions\".+\n?/gm, '') // Remove permissions
     .replace(/[\}\)\]]\,(?!\s*?[\{\[\"\'\w])/g, '}'); // Remove trailing commas if any left after json blocks
 };
 
 pathsToSanitize.forEach(async (i) => {
-  const directory = `../quickstarts/${i}/dashboards/`;
+  const directory = path.resolve('..', 'quickstarts', `${i}`, 'dashboards');
+  console.log(directory);
   const files = await readdir(directory, { withFileTypes: true });
   const jsonFiles = files.filter((file) => file.name.includes('.json'));
 
@@ -28,7 +30,7 @@ pathsToSanitize.forEach(async (i) => {
   }
 
   for (let file of jsonFiles) {
-    const filePath = directory + file.name;
+    const filePath = path.resolve(directory, file.name);
     const quickStart = fs.readFileSync(filePath, { encoding: 'utf8' });
     const cleanFile = sanitizeDashboard(quickStart);
 
