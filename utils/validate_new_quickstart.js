e98a13093101ff96b28858f61be5231f176472f6
Lucia Brammer <lbrammer@newrelic.com>
2021-12-20 15:15:46 -0800

    feat: Return uniqueQuickstartFilenames

--- utils/validate_new_quickstart.js
+++ utils/validate_new_quickstart.js
@@ -1,11 +1,23 @@
 'use strict';
-
-const {fetchPaginatedGHResults} = require('./github-api-helpers') 
+const { fetchPaginatedGHResults } = require('./github-api-helpers');
 
 const url = process.argv[2];
 
-Promise.resolve(fetchPaginatedGHResults(url, process.env.GITHUB_TOKEN)).then(response => {
-    console.log('return', response);
-})
+Promise.resolve(fetchPaginatedGHResults(url, process.env.GITHUB_TOKEN))
+  .then((response) => {
+    const uniqueQuickstartFilenames = response
+      .filter(({ filename }) => {
+        return (
+          filename.startsWith('quickstarts/') && filename.endsWith('config.yml')
+        );
+      })
+      .map(({ filename }) => filename);
 
-process.exit(0)
+    process.exit(0);
+  })
+  .catch(() => {
+    throw new Error(
+      `Github API returned status ${resp.code} - ${resp.message}`
+    );
+  });
+// path.resolve(basePath, '../quickstarts/**/config.yml');
