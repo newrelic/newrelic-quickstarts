99129bca761c2eb203599349f444e8d161619671
Lucia Brammer <lbrammer@newrelic.com>
2021-12-20 16:59:46 -0800

    chore: Isolate parent directories in live PR with config.yml

--- utils/validate_new_quickstart.js
+++ utils/validate_new_quickstart.js
@@ -1,23 +1,65 @@
 'use strict';
 const { fetchPaginatedGHResults } = require('./github-api-helpers');
+const path = require('path');
+const glob = require('glob');
 
+const EXCLUDED_DIRECTORY_PATTERNS = [
+  'node_modules/**',
+  'utils/**',
+  'docs/**',
+  '*',
+];
 const url = process.argv[2];
 
+const getQuickstartFilePaths = (basePath) => {
+  const options = {
+    ignore: EXCLUDED_DIRECTORY_PATTERNS.map((d) => path.resolve(basePath, d)),
+  };
+
+  const yamlFilePaths = [
+    ...glob.sync(
+      path.resolve(basePath, '../quickstarts/**/config.yaml'),
+      options
+    ),
+    ...glob.sync(
+      path.resolve(basePath, '../quickstarts/**/config.yml'),
+      options
+    ),
+  ];
+
+  return yamlFilePaths;
+};
+
+const hasConfig = ({ filename }) =>
+  (filename.startsWith('quickstarts/') && filename.endsWith('/config.yml')) ||
+  (filename.startsWith('quickstarts/') && filename.endsWith('/config.yaml'));
+
+// console.log(getQuickstartFilePaths(process.cwd()).sort());
+const getParentQuickstart = (filename) => {
+  console.log(filename);
+};
 Promise.resolve(fetchPaginatedGHResults(url, process.env.GITHUB_TOKEN))
   .then((response) => {
-    const uniqueQuickstartFilenames = response
-      .filter(({ filename }) => {
-        return (
-          filename.startsWith('quickstarts/') && filename.endsWith('config.yml')
-        );
-      })
+    const uniqueQuickstartConfigs = response
+      .filter(hasConfig)
       .map(({ filename }) => filename);
-
+    let uniqueQuickstarts = uniqueQuickstartConfigs.map((filename) => {
+      const splitFilePath = filename.split('/');
+      return splitFilePath[splitFilePath.length - 2];
+    });
+    // console.log(uniqueQuickstarts);
+    // console.log(uniqueQuickstartConfigs);
+    response.forEach(({ filename }) => {
+      uniqueQuickstarts.forEach((quickstart) => {
+        console.log(`${quickstart}, ${filename}`);
+        if (!filename.includes(quickstart)) {
+          getParentQuickstart(filename);
+        }
+      });
+    });
     process.exit(0);
   })
-  .catch(() => {
-    throw new Error(
-      `Github API returned status ${resp.code} - ${resp.message}`
-    );
+  .catch((error) => {
+    throw new Error(`Github API returned ${error.message}`);
   });
 // path.resolve(basePath, '../quickstarts/**/config.yml');
