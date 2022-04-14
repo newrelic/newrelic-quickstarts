d4f73a5695361a531d5f76d02b762bbee5920d39
Zack Stickles <zstickles@newrelic.com>
2022-04-14 11:17:48 -0700

    chore: added generic types for working with NerdGraph

---
+++ utils/types/nerdgraph.ts
@@ -0,0 +1,35 @@
+interface NerdGraphError {
+  extensions: {
+    argumentPath: string[];
+  };
+}
+
+/**
+ * A response from NerdGraph (including possible errors). Requires an
+ * expected shape for the response specific to the NerdGraph API call.
+ *
+ * @example
+ * const resp: NerdGraphResponse<{ name: string }> = {
+ *   // ...
+ * };
+ */
+export interface NerdGraphResponse<T> {
+  data: T & {
+    errors?: NerdGraphError[];
+  };
+}
+
+/**
+ * A request to NerdGraph. An optional type for the `variables` can be
+ * provided.
+ *
+ * @example
+ * const request: NerdGraphRequest<{ name: string }> = {
+ *   queryString: 'your-query-here',
+ *   variables: { name: 'Bob' },
+ * };
+ */
+export interface NerdGraphRequest<T = {}> {
+  queryString: string;
+  variables?: T;
+}
