f29ecf9dbb2dc8e7866fbcf869af4a8156fa3588
Tabatha Zeitke <tzeitke@newrelic.com>
2022-04-21 16:08:28 -0700

    feat: add validation call to Nerdgraph

--- utils/create-validate-data-sources.ts
+++ utils/create-validate-data-sources.ts
@@ -3,6 +3,7 @@ import {
   fetchPaginatedGHResults,
   filterDataSources,
 } from './github-api-helpers';
+import { chunk, fetchNRGraphqlResults, translateMutationErrors } from './nr-graphql-helpers';
 import {
   passedProcessArguments,
   readYamlFile,
@@ -19,6 +20,9 @@ import {
   DataSourceMutationVariable,
 } from './types/DataSourceMutationVariable';
 import { GITHUB_RAW_BASE_URL } from './constants';
+import { createValidateUpdateInstallPlan } from './create-validate-install-plans';
+import { NerdGraphResponseWithLocalErrors } from './types/nerdgraph';
+import { CUSTOM_EVENT, track } from './newrelic/customEvent';
 
 const gql = String.raw;
 const DATA_SOURCE_MUTATION = gql`
@@ -162,13 +166,66 @@ export const parseDataSource = (
     },
   };
 };
-// 1. fetch github files for pull request
-// 2. filter files to data source only
-// 3. read the data source files
-// 4. transform data source to mutation variable
-// 5. submit to nerdgraph
-// 6. if error, output error message
-//
+
+/**
+ * Validates for an array of data source filenames
+ * @param {Array} dataSourceFiles - Array containing data sources file names.
+ * @return {Promise.<Boolean>} - Boolean value indicating whether all files were validated
+ */
+const createValidateUpdateDataSources = async (
+  dataSourceFiles: { variables: DataSourceMutationVariable, filePath: string }[]
+): Promise<boolean> => {
+  type GraphQLResponse = NerdGraphResponseWithLocalErrors<
+    DataSourceMutationResponse
+  > & {
+    filePath: string;
+  };
+
+  const chunkedDataSourceRequests = chunk(dataSourceFiles, 5);
+
+  let graphqlResponses: GraphQLResponse[] = [];
+  // using a For Of loop so that it respects the `await`
+  for (const reqChunk of chunkedDataSourceRequests) {
+    const chunkRes = await Promise.all(
+      reqChunk.map(async ({ variables, filePath }) => {
+        const { data, errors } = await fetchNRGraphqlResults<
+          DataSourceMutationVariable,
+          DataSourceMutationResponse
+        >({
+          queryString: DATA_SOURCE_MUTATION,
+          variables,
+        });
+
+        return { data, filePath, errors };
+      })
+    );
+    graphqlResponses = [...graphqlResponses, ...chunkRes];
+  }
+
+  let hasFailed = false;
+
+  graphqlResponses.forEach(({ errors, filePath }) => {
+    if (errors && errors.length > 0) {
+      hasFailed = true;
+      translateMutationErrors(errors, filePath);
+    }
+  });
+
+  return hasFailed;
+};
+
+/**
+ * @param {boolean} hasFailed if the validation or submission has failed
+ * @param {boolean} isDryRun - true for validation, false for submission
+ */
+const recordCustomNREvent = async (hasFailed: boolean, isDryRun: boolean) => {
+  const status = hasFailed ? 'failed' : 'success';
+  const event = isDryRun
+    ? CUSTOM_EVENT.VALIDATE_DATA_SOURCES
+    : CUSTOM_EVENT.UPDATE_DATA_SOURCES;
+
+  await track(event, { status });
+};
 
 const main = async () => {
   const [GITHUB_API_URL, isDryRun] = passedProcessArguments();
@@ -186,8 +243,21 @@ const main = async () => {
   );
 
   const dataSourceConfigs = readDataSourceFiles(dataSourceConfigPaths);
-  console.log(JSON.stringify(parseDataSource(dataSourceConfigs[0]), null, 2));
-};
+
+  const mutationVariablesAndFilePaths = dataSourceConfigs.map((config) => {
+    return {
+      variables: parseDataSource(config, isDryRun === 'true'),
+      filePath: config.filePath,
+    }
+  })
+
+  const hasFailed = await createValidateUpdateDataSources(mutationVariablesAndFilePaths);
+  await recordCustomNREvent(hasFailed, isDryRun === 'true');
+
+  if (hasFailed) {
+    process.exit(1);
+  }
+}
 
 if (require.main === module) {
   main();
