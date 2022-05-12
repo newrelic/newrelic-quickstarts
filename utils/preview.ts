b924214b2e9c567b42fe51595af3d2c11db1038e
Mickey Ryan <mickeyryan@newrelic.com>
2022-05-12 17:30:53 -0400

    NR-2229 Add array of file paths to response

--- utils/preview.ts
+++ utils/preview.ts
@@ -1,22 +1,36 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+import * as express from 'express';
+
+// Types
+import { Request, Response } from 'express';
+
+// Modules
+import * as fs from 'fs';
+import * as path from 'path';
+import * as glob from 'glob';
+
+// App
 const app = express();
 const port: number = 3000;
 
 const QUICKSTART_PATH: string = process.argv[2];
 
+/*
+  Validates that the directory path provided exists
+*/
 fs.access(`../quickstarts/${QUICKSTART_PATH}`, (error: any) => {
   if (error) {
     throw new Error(`Path: ${QUICKSTART_PATH} does not exist.`);
-  } else {
-    const quickStartPath = path.join(path.resolve(__dirname, '..'), `/quickstarts/${QUICKSTART_PATH}`);
-  
-    app.use(express.static(quickStartPath));
-
-    app.listen(port, () => {
-      console.log("\x1b[32m", `Preview here: http://localhost:3000/config.yml`);
-    });
   }
 });
 
+app.use(express.static(path.join(path.resolve(__dirname, '..'), `/quickstarts/${QUICKSTART_PATH}`)));
+
+app.get('/', (req: Request, res: Response) => {
+  const response = glob.sync(path.join(path.resolve(__dirname, '..'), `/quickstarts/${QUICKSTART_PATH}/**/*.*`));
+  
+  res.send(response);
+});
+
+app.listen(port, () => {
+  console.log("\x1b[32m", `Use this to get a list of your files! http://localhost:3000`);
+});
