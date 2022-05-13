7ae09e692f4878eb3c7b086625241b2db8439594
Mickey Ryan <mickeyryan@newrelic.com>
2022-05-13 11:46:27 -0400

    NR-2229 Local server for quickstart preview

--- utils/preview.ts
+++ utils/preview.ts
@@ -4,33 +4,68 @@ import * as express from 'express';
 import { Request, Response } from 'express';
 
 // Modules
-import * as fs from 'fs';
 import * as path from 'path';
 import * as glob from 'glob';
 
 // App
 const app = express();
-const port: number = 3000;
 
-const QUICKSTART_PATH: string = process.argv[2];
+const port: string = process.env.PORT || '3000';
+const QUICKSTART_DIRECTORY: string = process.argv[2];
+const PARENT_DIRECTORY: string = path.resolve(__dirname, '..');
 
 /*
-  Validates that the directory path provided exists
+    Validates that the directory path provided exists
 */
-fs.access(`../quickstarts/${QUICKSTART_PATH}`, (error: any) => {
-  if (error) {
-    throw new Error(`Path: ${QUICKSTART_PATH} does not exist.`);
-  }
-});
-
-app.use(express.static(path.join(path.resolve(__dirname, '..'), `/quickstarts/${QUICKSTART_PATH}`)));
-
-app.get('/', (req: Request, res: Response) => {
-  const response = glob.sync(path.join(path.resolve(__dirname, '..'), `/quickstarts/${QUICKSTART_PATH}/**/*.*`));
+const validatePath = async (quickstartDirectory: string) => {
+  const configFile = glob.sync(path.join(PARENT_DIRECTORY, `/quickstarts/${quickstartDirectory}/config.+(yml|yaml)`));
+  const fullPath = path.join(PARENT_DIRECTORY, `/quickstarts/${quickstartDirectory}`);
   
-  res.send(response);
-});
+  if (!configFile || configFile.length !== 1) {
+    console.error(`Could not find a config.yml or config.yaml at ${fullPath}.`);
+    process.exit(1);
+  }
+};
 
-app.listen(port, () => {
-  console.log("\x1b[32m", `Use this to get a list of your files! http://localhost:3000`);
-});
+const removePathPrefixes = (paths: string[]) => {
+  return paths.map(filePath => filePath.slice(filePath.indexOf('/quickstarts')).slice(1));
+};
+
+const main = async () => {
+    // Validate whether param was provided
+  if (!process.argv[2]) {
+    console.error("You must provide a directory path to the quickstart. Example: yarn preview aws/amazon-athena");
+    process.exit(1);
+  }
+
+  /*
+        Set to localhost for dev purposes TODO: Set to instant observability later
+  */
+  let PREVIEW_LINK = 'https://localhost:8000/preview?local=true';
+
+  if (port !== '3000') {
+    PREVIEW_LINK += `&port=${port}`;
+  }
+
+  await validatePath(QUICKSTART_DIRECTORY);
+
+  app.use(`/quickstarts/${QUICKSTART_DIRECTORY}`, express.static(path.join(PARENT_DIRECTORY, `/quickstarts/${QUICKSTART_DIRECTORY}`)));
+
+  app.get('/', (req: Request, res: Response) => {
+    let response = glob.sync(path.join(PARENT_DIRECTORY, `/quickstarts/${QUICKSTART_DIRECTORY}/**/*.*`))
+                      
+    response = removePathPrefixes(response);                  
+
+    res.send(response);
+  });
+
+  app.listen(port, () => {
+
+    /*
+        Set to localhost for dev purposes TODO: Set to instant observability later
+    */
+    console.log("\x1b[32m", `Now serving your output on: http://localhost:${port} \n You can view your preview at: ${PREVIEW_LINK}`);
+  });
+};
+
+main();
