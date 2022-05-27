6c20d5a6cb21ab2b064c560247fe2364365ea422
Tabatha Zeitke <tzeitke@newrelic.com>
2022-05-27 16:05:33 -0700

    feat: create new dashboard directories

--- utils/migration_scripts/move-dashboards.ts
+++ utils/migration_scripts/move-dashboards.ts
@@ -5,6 +5,11 @@ import {
   DashboardFileAndPath,
 } from '../lib/dashboards';
 
+import { getQuickstartFromFilename } from '../helpers';
+
+import * as path from 'path';
+import * as fs from 'fs';
+
 const groupDuplicates = (
   dashboards: DashboardFileAndPath[]
 ): DashboardFileAndPath[][] => {
@@ -24,12 +29,45 @@ const groupDuplicates = (
   return Object.values(dupBuckets);
 };
 
+const setupDashboardDir = () => {
+  const dashboardDirPath = path.resolve(__dirname, '../..', 'dashboards');
+  if (!fs.existsSync(dashboardDirPath)) {
+    fs.mkdirSync(dashboardDirPath)
+  }
+}
+
 const main = () => {
+  setupDashboardDir();
   const paths = getAllDashboardPaths();
 
   const dashboardsAndPaths = paths.map(readDashboardFile);
   const grouped = groupDuplicates(dashboardsAndPaths);
-  console.log(grouped);
+  // console.log(grouped);
+
+  grouped.forEach(group => {
+    // determine new location for dashboard
+    const currentDashLocation = group[0].path;
+    const newDashLocation = path.dirname(currentDashLocation).split('/').pop()!;
+    // cleanup file path name 
+    const cleanedDashLocation = newDashLocation.replace(/_/g, "-").toLowerCase();
+    // create new dashboard directory
+    let fullDashboardPath = path.resolve(__dirname, '../..', 'dashboards', cleanedDashLocation);
+    // if already exists, suffix directory name with quickstart slug
+    if (fs.existsSync(fullDashboardPath)) {
+      const quickstartLocation = getQuickstartFromFilename(currentDashLocation)?.split("/quickstarts/").pop()?.replace(/\//g, "-");
+
+      fullDashboardPath = fullDashboardPath + "-" + quickstartLocation;
+    }
+    fs.mkdirSync(fullDashboardPath)
+    console.log(fullDashboardPath)
+    // get all dashboard files (screenshots)
+    // create new files in this location
+
+    // loop through group 
+    //    update each quickstart config to reference the new location of dashboard
+    //    delete old dashboard files
+    //    
+  })
 };
 
 main();
