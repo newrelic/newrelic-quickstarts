1cbe03475ab36e1e0b5e3fbe7e53ab0d1d2bd81c
Joe Gregory <jgregory@newrelic.com>
2022-05-31 16:47:35 -0700

    chore: Improved docs for functions

--- utils/lib/dashboards.ts
+++ utils/lib/dashboards.ts
@@ -14,18 +14,33 @@ interface Dashboard {
 
 export type DashboardFileAndPath = FilePathAndContents<Dashboard>;
 
-export const getAllDashboardPaths = () => {
+/**
+ * Wrapper function retreives all dashboard paths in the top level
+ * of `quickstarts/`.
+ * @returns - Array of file paths to all dashboards in `quickstarts/`
+ */
+export const getAllDashboardPaths = (): string[] => {
   return findQuickstartComponentConfiguration('dashboards');
 };
 
-// .sjon
-// .png
-// assets/iconm.svg
+/**
+ * Lib function fetches all file paths related to a dashboard that
+ * are assets to the dashboard itself. This does not include 
+ * JSON files.
+ * @param dashFilePath - absolute file path to dashboard directory.
+ * @returns - Array of file paths of assets to a dashboard
+ */
 export const getDashboardScreenshotsPath = (dashFilePath: string): string[] => {
   const dashDir = path.dirname(dashFilePath);
   return glob.sync(path.resolve(dashDir, '*.!(json)'));
 };
 
+/**
+ * Wrapper function reads the contents of a dashboard
+ * config file.
+ * @param dashFilePath - file path of dashboard config file 
+ * @returns - An object containing path and content of a dashboard config file.  
+ */
 export const readDashboardFile = (
   dashFilePath: string
 ): DashboardFileAndPath => {
