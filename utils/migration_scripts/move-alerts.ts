cda8dc06b761727489389e408d3a55c735b08066
Zack Stickles <zstickles@newrelic.com>
2022-05-31 16:50:23 -0700

    chore: removing old alerts and updating config

--- utils/migration_scripts/move-alerts.ts
+++ utils/migration_scripts/move-alerts.ts
@@ -1,5 +1,8 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import * as glob from 'glob';
+
+import { RmDirOptions } from 'fs';
 
 import { findMainQuickstartConfigFiles } from '../helpers';
 
@@ -13,24 +16,61 @@ const setupAlertPolicyDir = () => {
 };
 
 const main = () => {
+  setupAlertPolicyDir();
+
   // find all alerts with alert conditions
   const quickstartsWithAlerts = findMainQuickstartConfigFiles()
     .map(path.dirname)
     .filter((dir) => fs.existsSync(path.join(dir, 'alerts')));
 
   // make an alert-policy directory for each quickstart
-  // move the alert conditions into the new directory
-  setupAlertPolicyDir();
-  for (const quickstartDirName of quickstartsWithAlerts) {
-    const oldAlertsDir = path.join(quickstartDirName, 'alerts');
-    const newAlertPolicyDir = path.join(DIR_PATH, quickstartDirName);
-    // FIXME: this is not working as intended...
+  // copy the alert conditions into the new directory
+  const newAlertPolicyDirs = quickstartsWithAlerts.map((quickstartDirPath) => {
+    const quickstartName = path.basename(quickstartDirPath);
+    const oldAlertsDir = path.join(quickstartDirPath, 'alerts');
+    const newAlertPolicyDir = path.join(DIR_PATH, quickstartName);
+
     fs.cpSync(oldAlertsDir, newAlertPolicyDir, { recursive: true });
-  }
+
+    return newAlertPolicyDir;
+  });
 
   // remove the `---` from the top / bottom of the condition files
-  // update the quickstart config files to reference the policy
-  // remove the alerts from the quickstart directory
+  for (const alertPolicyDir of newAlertPolicyDirs) {
+    for (const filename of fs.readdirSync(alertPolicyDir)) {
+      const filepath = path.join(alertPolicyDir, filename);
+      const fileContent = fs.readFileSync(filepath, { encoding: 'utf8' });
+      const updatedFile = fileContent.replace(/---\n/g, '');
+
+      fs.writeFileSync(filepath, updatedFile, 'utf8');
+    }
+  }
+
+  for (const quickstartDirPath of quickstartsWithAlerts) {
+    // update the quickstart config files to reference the policy
+    const filePaths = glob.sync(path.join('/', quickstartDirPath, 'config.*'));
+
+    if (!Array.isArray(filePaths) || filePaths.length === 0) {
+      return;
+    }
+
+    const configFilePath = filePaths.pop();
+
+    const quickstartName = path.basename(quickstartDirPath);
+    const rawConfig = fs.readFileSync(configFilePath!, { encoding: 'utf-8' });
+    const updatedConfig = rawConfig.concat(
+      '\nalert-policies:\n  - ',
+      quickstartName
+    );
+
+    fs.writeFileSync(configFilePath!, updatedConfig, 'utf-8');
+
+    // remove the alerts from the quickstart directory
+    fs.rmdirSync(path.join('/', quickstartDirPath, 'alerts/'), {
+      recursive: true,
+      force: true,
+    } as RmDirOptions);
+  }
 };
 
 main();
