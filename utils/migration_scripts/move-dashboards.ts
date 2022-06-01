8838ac8146d5f506b94de4edebf277097d8c57eb
Joe Gregory <jgregory@newrelic.com>
2022-06-01 12:00:10 -0700

    chore: update documentation and code refactor
    * Moved logic in main to a dedicated function for easier readability
    * Update comments and JSDocs

--- utils/migration_scripts/move-dashboards.ts
+++ utils/migration_scripts/move-dashboards.ts
@@ -13,6 +13,14 @@ import * as fs from 'fs';
 import { RmDirOptions } from 'fs';
 import * as glob from 'glob';
 
+/**
+ * Function handles creating an object that groups duplicate and unique
+ * dashboards to their respective key. 
+ *
+ * `key: dashboard pages`
+ * @param dashboards - Array of FilePathAndContents  
+ * @returns - 2D array of grouped dashboard paths 
+ */
 const groupDuplicates = (
   dashboards: DashboardFileAndPath[]
 ): DashboardFileAndPath[][] => {
@@ -32,6 +40,10 @@ const groupDuplicates = (
   return Object.values(dupBuckets);
 };
 
+/**
+ * Function creates a `dashboards/` directory if one does not exist
+ * in the file system
+ */
 const setupDashboardDir = () => {
   const dashboardDirPath = path.resolve(__dirname, '../..', 'dashboards');
   if (!fs.existsSync(dashboardDirPath)) {
@@ -39,6 +51,12 @@ const setupDashboardDir = () => {
   }
 };
 
+/**
+ * Function handles creating a new dashboard location that points
+ * to the top level of the `dashboards/` directory.
+ * @param currentDashLocation - The path of the dashboard
+ * @returns - The new dashboard location to the top level of `dashboards/` 
+ */
 const getNewDashboardLocation = (currentDashLocation: string) => {
   const dashLocation = path.dirname(currentDashLocation).split('/').pop()!;
   // cleanup file path name
@@ -50,7 +68,7 @@ const getNewDashboardLocation = (currentDashLocation: string) => {
     'dashboards',
     cleanedDashLocation
   );
-  // if already exists, suffix directory name with quickstart slug
+  // if already exists, suffix new dashboard directory name with quickstart dirname
   if (fs.existsSync(newDashboardLocation)) {
     const quickstartLocation = getQuickstartFromFilename(currentDashLocation)
       ?.split('/quickstarts/')
@@ -62,6 +80,13 @@ const getNewDashboardLocation = (currentDashLocation: string) => {
   return newDashboardLocation;
 };
 
+/**
+ * Function handles creating a new dashbord location and copying the dashboard
+ * from `quickstarts/` to `dashboards/`.
+ *
+ * @param newDashboardLocation - The new path to the top level of `dashboards/`
+ * @param currentDashboardLocation - The old path to the dashboard in `quickstarts/`
+ */
 const copyFiles = (
   newDashboardLocation: string,
   currentDashboardLocation: string
@@ -72,6 +97,47 @@ const copyFiles = (
     path.join(newDashboardLocation, path.basename(currentDashboardLocation))
   );
 };
+
+type DirectoryAndDashTuple = [string, string[]][];
+
+const updateQuickstarts = (dirAndDashTuples: DirectoryAndDashTuple) => {
+  dirAndDashTuples.forEach(([quickstartDir, newDashboards])=> {
+    const filePaths = glob.sync(path.join('/', quickstartDir, 'config.*'));
+    if (!Array.isArray(filePaths) || filePaths.length === 0) {
+      return;
+    }
+
+    const configFilePath = filePaths.pop();
+
+    // get the new dashboard location
+    const newDashboardLocations = newDashboards.map((dashPath: string) =>
+      path.basename(dashPath)
+    );
+
+    // update the quickstart config
+    const rawConfig = fs.readFileSync(configFilePath!, { encoding: 'utf-8' });
+
+    const updatedRawConfig = rawConfig.concat(
+      '\n',
+      'dashboards:',
+      '\n  - ',
+      newDashboardLocations.join('\n  - ')
+    );
+
+    // update impacted quickstart config file to add dashboard IDs
+    fs.writeFileSync(configFilePath!, updatedRawConfig, 'utf-8'); 
+
+
+    // delete the dashboard directory
+    fs.rmdirSync(path.join('/', quickstartDir, 'dashboards/'), {
+      recursive: true,
+      force: true,
+    } as RmDirOptions);
+  })
+
+
+
+}
 
 const main = () => {
   setupDashboardDir();
@@ -120,42 +186,8 @@ const main = () => {
       });
   });
 
-  // loop through all the quickstarts
-
-  Object.entries(quickstartToUpdate).forEach(([quickstartDir, newDashboards]) => {
-    const filePaths = glob.sync(path.join('/', quickstartDir, 'config.*'));
-    if (!Array.isArray(filePaths) || filePaths.length === 0) {
-      return;
-    }
-
-    const configFilePath = filePaths.pop();
-
-    // get the new dashboard location
-    const newDashboardLocations = newDashboards.map((dashPath) =>
-      path.basename(dashPath)
-    );
-
-    // update the quickstart config
-    const rawConfig = fs.readFileSync(configFilePath!, { encoding: 'utf-8' });
-
-    const updatedRawConfig = rawConfig.concat(
-      '\n',
-      'dashboards:',
-      '\n  - ',
-      newDashboardLocations.join('\n  - ')
-    );
-
-    // update impacted quickstart config file to add dashboard IDs
-    fs.writeFileSync(configFilePath!, updatedRawConfig, 'utf-8'); 
-
-
-    // delete the dashboard directory
-    fs.rmdirSync(path.join('/', quickstartDir, 'dashboards/'), {
-      recursive: true,
-      force: true,
-    } as RmDirOptions);
-  })
-
+  // loop through all the quickstarts that need to be updated
+  updateQuickstarts(Object.entries(quickstartToUpdate))
 };
 
 main();
