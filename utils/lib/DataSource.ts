c717f825e341e55edc06ff470443a452a48f6158
Zack Stickles <zstickles@newrelic.com>
2022-06-07 16:51:28 -0700

    chore: added data source component methods
    This adds the methods for the data source component. It includes logic
    for producing the query variables for the quickstart mutation _and_ the
      data source mutation.
    
    We should make a follow-up PR to cleanup / refactor the helper function
    and workflow script.

--- utils/lib/DataSource.ts
+++ utils/lib/DataSource.ts
@@ -1,35 +1,136 @@
-import Component from './Component'
-import { readQuickstartFile } from '../helpers'
-import type { DataSourceConfig } from '../types/DataSourceConfig'
+import * as path from 'path';
+import * as glob from 'glob';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
-class DataSource extends Component<DataSourceConfig> {
+import Component from './Component';
+import { GITHUB_RAW_BASE_URL } from '../constants';
+import { removeRepoPathPrefix } from './helpers';
+
+import type {
+  DataSourceConfig,
+  DataSourceConfigInstallDirective,
+} from '../types/DataSourceConfig';
+import type { DataSourceInstallDirectiveInput } from '../types/DataSourceMutationVariable';
+
+class DataSource extends Component<DataSourceConfig, string> {
   /**
-   * Returns the file path from the top level of component
-   * @returns - filepath from top level directory.
+   * @returns Filepath for the configuration file (from top-level directory).
    */
   getConfigFilePath() {
-    this.configPath = `data-sources/${this.name}`;
-    return this.configPath;
+    const filePaths = glob.sync(
+      path.join(this.basePath, 'data-sources', this.localPath, '*.{yml|yaml}')
+    );
+
+    if (!Array.isArray(filePaths) || filePaths.length !== 1) {
+      this.isValid = false;
+      return '';
+    }
+
+    return removeRepoPathPrefix(filePaths[0]);
   }
 
   /**
-   * Read and parse a JSON file
-   * @param filePath - The path to the JSON file
-   * @returns - An object containing the path and contents of the file
+   * @returns The YAML configuration for the data source.
    */
   getConfigContent() {
-    this.config = readQuickstartFile<DataSourceConfig>(this.configPath)
-    return this.config;
+    if (!this.isValid) {
+      return this.config;
+    }
+
+    try {
+      const file = fs.readFileSync(this.fullPath);
+
+      return yaml.load(file.toString('utf-8')) as DataSourceConfig;
+    } catch (e) {
+      console.log('Unable to parse quickstart config', this.configPath, e);
+      this.isValid = false;
+
+      return this.config;
+    }
   }
 
   /**
-   * TODO: implement validation
+   * Get the variables for the **Quickstart** mutation.
+   *
+   * @returns The ID for this data source
    */
-  validate() {
-    this.isValid = true;
-    return true;
+  getMutationVariables() {
+    return this.config.id;
+  }
+
+  /**
+   * Get the variables for the **Data Source** mutation.
+   *
+   * @returns Mutation variables
+   */
+  public getDataSourceMutationVariables() {
+    const { displayName, categoryTerms, keywords, description } = this.config;
+
+    return {
+      displayName: displayName.trim(),
+      icon: this._getIconUrl(),
+      install: this._parseInstall(),
+      categoryTerms: categoryTerms && categoryTerms.map((t) => t.trim()),
+      keywords: keywords && keywords.map((k) => k.trim()),
+      description: description && description.trim(),
+    };
+  }
+
+  /**
+   * Helper method to get the image URL for the icon.
+   */
+  private _getIconUrl() {
+    const { icon } = this.config;
+    const dirName = path.dirname(this.configPath);
+    const relDirName = removeRepoPathPrefix(dirName);
+
+    return `${GITHUB_RAW_BASE_URL}/${relDirName}/${icon}`;
+  }
+
+  /**
+   * Helper method to parse the primary and fallback install directives.
+   */
+  private _parseInstall() {
+    const { install } = this.config;
+
+    return {
+      primary: this._parseInstallDirective(install.primary),
+      fallback:
+        install.fallback && this._parseInstallDirective(install.fallback),
+    };
+  }
+
+  /**
+   * Helper method that returns the directive, based on it's type.
+   */
+  private _parseInstallDirective(
+    directive: DataSourceConfigInstallDirective
+  ): DataSourceInstallDirectiveInput {
+    if ('link' in directive) {
+      const { url } = directive.link;
+
+      return {
+        link: {
+          url: url?.trim() ?? '',
+        },
+      };
+    }
+
+    if ('nerdlet' in directive) {
+      const { nerdletId, nerdletState, requiresAccount } = directive.nerdlet;
+
+      return {
+        nerdlet: {
+          nerdletId: nerdletId.trim() ?? '',
+          nerdletState: nerdletState && JSON.stringify(nerdletState),
+          requiresAccount: requiresAccount,
+        },
+      };
+    }
+
+    return directive;
   }
 }
 
 export default DataSource;
-
