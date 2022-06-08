9f928da095e567ce6f5e57432125bad71d37c840
Zack Stickles <zstickles@newrelic.com>
2022-06-08 11:34:16 -0700

    refactor: Generalize the quickstart component methods

--- utils/lib/Quickstart.ts
+++ utils/lib/Quickstart.ts
@@ -1,41 +1,102 @@
-import Alert from './Alert'
-import Component from './Component'
-import Dashboard from './Dashboard'
-import DataSource from './DataSource'
-import { readQuickstartFile } from '../helpers'; 
-import type { QuickstartConfig } from '../types/QuickstartConfig'
+import Alert from './Alert';
+import Component from './Component';
+import Dashboard from './Dashboard';
+import DataSource from './DataSource';
+import { readQuickstartFile } from '../helpers';
+import type { QuickstartConfig } from '../types/QuickstartConfig';
+import { QuickstartMutationVariable } from '../types/QuickstartMutationVariable';
 
-abstract class Quickstart extends Component<QuickstartConfig> {
-  public dashboards: Dashboard[] = [];
-  public alerts: Alert[] = [];
-  public dataSources: DataSource[] = [];
+type ComponentType = typeof Alert | typeof Dashboard | typeof DataSource;
+
+interface ComponentTypes {
+  configKey: string;
+  mutationKey: string;
+  ctor: ComponentType;
+}
+
+const COMPONENT_TYPES: ComponentTypes[] = [
+  { configKey: 'alertPolicies', mutationKey: 'alertConditions', ctor: Alert },
+  { configKey: 'dashboards', mutationKey: 'dashboards', ctor: Dashboard },
+  {
+    configKey: 'dataSourceIds',
+    mutationKey: 'dataSourceIds',
+    ctor: DataSource,
+  },
+];
+
+abstract class Quickstart extends Component<
+  QuickstartConfig,
+  QuickstartMutationVariable
+> {
+  public components: (Dashboard | Alert | DataSource)[];
 
   constructor(name: string) {
     super(name);
-    this.dashboards = this.getDashboards();
-    this.alerts = this.getAlerts();
-    this.dataSources = this.getDataSources();
+
+    this.components = this.getComponents();
   }
 
-  getDashboards(): Dashboard[] {
-    this.dashboards = this.config?.contents[0]?.dashboards?.map(
-      (dashboardName) => new Dashboard(dashboardName)
-    ) ?? [];
-    return this.dashboards;
+  getComponents() {
+    return COMPONENT_TYPES.flatMap((componentType) => {
+      const componentConfig = this.config?.[
+        componentType.configKey as keyof QuickstartConfig
+      ] as string[];
+
+      return (
+        componentConfig.flatMap(
+          (name: string) => new componentType.ctor(name)
+        ) ?? []
+      );
+    });
   }
 
-  getAlerts(): Alert[] {
-    this.alerts = this.config?.contents[0]?.alertPolicies?.map(
-      (alertPolicy) => new Alert(alertPolicy)
-    ) ?? [];
-    return this.alerts
+  getMutationVariables() {
+    const variables = {
+      /* ...name, id, etc */
+    };
+
+    const variablesWithComponents = COMPONENT_TYPES.reduce((vars, type) => {
+      const componentsOfType = this.components.filter(
+        (c) => c instanceof type.ctor
+      );
+
+      // if we don't have anything for this component type, just return the variables
+      if (!componentsOfType.length) {
+        return vars;
+      }
+
+      // otherwise, we have some components of this type
+      // add them to the mutation variables
+      const variablesForType = componentsOfType.map((c) =>
+        c.getMutationVariables()
+      );
+
+      return {
+        ...vars,
+        [type.mutationKey]: variablesForType,
+      };
+    }, variables);
+
+    // FIXME: properly get _all_ of the quickstart variables, not just the component ones
+    return variablesWithComponents as QuickstartMutationVariable;
   }
 
-  getDataSources(): DataSource[] {
-    this.dataSources = this.config?.contents[0]?.dataSourceIds?.map(
-      (dataSourceId) => new DataSource(dataSourceId)
-    ) ?? [];
-    return this.dataSources
+  validate() {
+    const invalidComponents = this.components.filter((component) => {
+      return !component.isValid;
+    });
+
+    if (invalidComponents.length) {
+      console.log('The following components are not valid:');
+
+      for (const { localPath } of invalidComponents) {
+        console.log(`\t ${localPath}`);
+      }
+
+      this.isValid = false;
+    }
+
+    // TODO: validate the quickstart itself
   }
 
   /**
@@ -43,8 +104,9 @@ abstract class Quickstart extends Component<QuickstartConfig> {
    * @returns - filepath from top level directory.
    */
   getConfigFilePath() {
-    this.configPath =`quickstarts/${this.name}`;
-    return this.configPath
+    // TODO: yml or yaml
+    this.configPath = `quickstarts/${this.localPath}/config.yml`;
+    return this.configPath;
   }
 
   /**
@@ -53,18 +115,10 @@ abstract class Quickstart extends Component<QuickstartConfig> {
    * @returns - An object containing the path and contents of the file
    */
   getConfigContent() {
-    this.config = readQuickstartFile<QuickstartConfig>(this.configPath)
-    return this.config
-  }
-
-  /**
-   * TODO: implement validation
-   */
-  validate() {
-    this.isValid = true;
-    return true;
+    // TODO: read the yaml here
+    this.config = readQuickstartFile<QuickstartConfig>(this.configPath);
+    return this.config;
   }
 }
 
 export default Quickstart;
-
