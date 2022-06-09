750ed80fbbbe97714792696e408fbcdad1361446
Zack Stickles <zstickles@newrelic.com>
2022-06-08 17:12:34 -0700

    wip: starting to set up alert policies

--- utils/lib/Alert.ts
+++ utils/lib/Alert.ts
@@ -1,22 +1,24 @@
 import * as path from 'path';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 import * as glob from 'glob';
 
 import Component from './Component';
 import { removeRepoPathPrefix, getAssetSourceUrl } from './helpers';
+
 import type {
   AlertType,
   QuickstartAlertInput,
 } from '../types/QuickstartMutationVariable';
 import type { QuickstartConfigAlert } from '../types/QuickstartConfig';
 
-class Alert extends Component<QuickstartConfigAlert, QuickstartAlertInput> {
+class Alert extends Component<QuickstartConfigAlert[], QuickstartAlertInput> {
   /**
-   * Returns the file path from the top level of component
-   * @returns - filepath from top level directory.
+   * Returns the **directory** for the alert policy
    */
   getConfigFilePath() {
     const filePaths = glob.sync(
-      path.join(this.basePath, 'alert-policies', this.localPath, '*.{yml|yaml}')
+      path.join(this.basePath, 'alert-policies', this.localPath)
     );
 
     if (!Array.isArray(filePaths) || filePaths.length !== 1) {
@@ -27,13 +29,38 @@ class Alert extends Component<QuickstartConfigAlert, QuickstartAlertInput> {
     return removeRepoPathPrefix(filePaths[0]);
   }
 
-  // FIXME: alert *policies* have multiple config YAML files...
   getConfigContent() {
-    return this._getYamlConfigContent();
+    if (!this.isValid) {
+      return this.config;
+    }
+
+    const filePaths = glob.sync(path.join(this.configPath, '*.{yml|yaml}'));
+
+    // if there are no YAML files in this directory, it's invalid
+    if (!Array.isArray(filePaths) || !filePaths.length) {
+      this.isValid = false;
+      return this.config;
+    }
+
+    // loop through all the YAML files and get their content
+    try {
+      return filePaths.map((filepath) => {
+        const file = fs.readFileSync(path.join(this.basePath, filepath));
+
+        return yaml.load(file.toString('utf-8')) as QuickstartConfigAlert;
+      });
+    } catch (e) {
+      console.log('Unable to parse YAML config', this.configPath, e);
+      this.isValid = false;
+
+      return this.config;
+    }
   }
 
+  // TODO: update this to work like adaptQuickstartAlertsInput
+  // NOTE: this.config is an **array** of configuration
   getMutationVariables() {
-    const { description, name, type } = this.config;
+    const { description, name, type } = this.config[0];
 
     return {
       description: description && description.trim(),
