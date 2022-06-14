7a24e4ceac6585046ab1e24b40c91d5e76e085f5
Joe Gregory <jgregory@ht79cr4dn2.lan>
2022-06-14 13:31:59 -0700

    chore: fix function to look at config name

--- utils/check_dashboard_name_uniqueness.ts
+++ utils/check_dashboard_name_uniqueness.ts
@@ -5,16 +5,16 @@ import {
 import Dashboard from './lib/Dashboard'
 
 /**
- * Returns any dashboards with matching names
+ * Returns any dashboards with matching dashboard names
  * @param dashboards - An array of objects containing the path and name of a dashboard
  * @returns - An array of matching values
  */
 const findMatchingDashboardNames = (
   dashboards: Array<Dashboard>
 ): Array<Dashboard> => {
-  return dashboards.reduce<Dashboard[]>((acc, { localPath, configPath }) => {
+  return dashboards.reduce<Dashboard[]>((acc, { localPath, config }) => {
     const duplicates = dashboards.filter(
-      (dashboard) => dashboard.localPath === localPath && dashboard.configPath !== configPath
+      (dashboard) => dashboard.localPath === localPath && dashboard.config.name !== config.name 
     );
 
     return [...new Set([...acc, ...duplicates])];
