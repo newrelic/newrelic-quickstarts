8886d7a83b066ec9def63d66d70465cda087b120
Joe Gregory <jgregory@ht79cr4dn2.lan>
2022-06-14 14:32:32 -0700

    chore: updated sanitized script for new class updates

--- utils/sanitize_dashboards.ts
+++ utils/sanitize_dashboards.ts
@@ -1,78 +1,46 @@
 import * as fs from 'fs';
-import * as path from 'path';
-// Removes the first two arguments passed by `node`, these aren't needed for this function to run - CM
-const pathsToSanitize = process.argv.slice(2);
+import Dashboard from './lib/Dashboard'; 
 
-type DirectoriesAndJsonFiles = {
-  directories: string[]; 
-  jsonFiles: string[];
-};
+const sanitizeDashboards = (pathsToSanitize: string[]) => {
+  pathsToSanitize.forEach((i) => {
+    const dashboard = new Dashboard(i);
 
-if (pathsToSanitize.length < 1)
-  console.log(
-    "You must pass a file path as an argument for the sanitize function to run \nExample: 'yarn sanitize-dashboard node-js/express'"
-  );
-
-const sanitizeDashboard = (
-  fileContent: string
-  ): string => {
-  return fileContent
-    .replace(/\"accountId\"\s*:\s*\d+/g, '"accountId": 0') // Anonymize account ID
-    .replace(
-      /\"linkedEntityGuids\"\s*:\s*\[([\t\n\r]*)?.*([\t\n\r]*)?\s*\]/g,
-      '"linkedEntityGuids": null'
-    ) // Set linkedEntityGuids to null
-    .replace(/^.+\"permissions\".+\n?/gm, '') // Remove permissions
-    .replace(/[\}\]]\,(?!\s*?[\{\[\"\'\w])/g, '}'); // Remove trailing commas if any left after json blocks
-};
-
-const checkDirForDashboardJson = (
-  directory: string
-  ): DirectoriesAndJsonFiles => {
-  const files = fs.readdirSync(directory, { withFileTypes: true });
-  
-  return {
-    directories: files
-      .filter((file) => file.isDirectory())
-      .map((file) => path.resolve(directory, file.name)),
-    jsonFiles: files
-      .filter((file) => file.name.includes('json'))
-      .map((file) => path.resolve(directory, file.name)),
-  };
-};
-
-pathsToSanitize.forEach((i) => {
-  const directory: string = path.resolve('..', 'quickstarts', `${i}`, 'dashboards');
-  const files: DirectoriesAndJsonFiles = checkDirForDashboardJson(directory);
-  let jsonFiles = files.jsonFiles;
-
-  for (let nestedDir of files.directories) {
-    let nestedFiles = checkDirForDashboardJson(nestedDir);
-    if (nestedFiles.jsonFiles.length > 0) {
-      jsonFiles = jsonFiles.concat(nestedFiles.jsonFiles);
+    if (!dashboard.isValid) {
+      console.log(
+        'No dashboard or .json files exist within this directory, skipping sanitization...'
+      );
+      return;
     }
-  }
 
-  if (jsonFiles.length < 1) {
-    console.log(
-      'No .json files exist within this directory, skipping sanitization...'
-    );
-    return;
-  }
+    const dashboardRawConfig: string = JSON.stringify(dashboard.config)
+    const sanitizedRawConfig: string = dashboard.sanitizeConfig();
 
-  for (let filePath of jsonFiles) {
-    const quickStart: string = fs.readFileSync(filePath, { encoding: 'utf8' });
-    const cleanFile: string = sanitizeDashboard(quickStart);
-
-    if (cleanFile !== quickStart) {
-      fs.writeFile(filePath, cleanFile, { encoding: 'utf8' }, function (err) {
+    if (dashboardRawConfig !== sanitizedRawConfig) {
+      fs.writeFile(dashboard.fullPath, sanitizedRawConfig, { encoding: 'utf8' }, function (err) {
         if (err) {
-          console.error(`Error updating ${filePath}`);
+          console.error(`Error updating ${dashboard.fullPath}`);
           process.exit(1);
         } else {
-          console.log(`=> Sanitized ${filePath}`);
+          console.log(`=> Sanitized ${dashboard.fullPath}`);
         }
       });
     }
-  }
-});
+  });
+
+}
+
+const main = () => {
+  const pathsToSanitize = process.argv.slice(2);
+
+  if (pathsToSanitize.length < 1)
+    console.log(
+      'You must pass a file path as an argument for the sanitize function to run.:',
+      "Example: 'yarn sanitize-dashboard node-js/express'"
+    );
+
+  sanitizeDashboards(pathsToSanitize);
+}
+
+if (require.main === module) {
+  main();
+}
