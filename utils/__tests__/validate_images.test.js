0b71ddd250a51f9a2382cc37d80e4a5f4ce73373
Joe Gregory <jgregory@ht79cr4dn2.lan>
2022-06-17 14:12:33 -0700

    chore: moved single test to validate_images.test and removed validate_images_on_file_system.test

--- utils/__tests__/validate_images.test.js
+++ utils/__tests__/validate_images.test.js
@@ -125,4 +125,14 @@ describe('Action: validate images', () => {
     expect(core.setFailed).toHaveBeenCalled();
     expect(global.console.warn).toHaveBeenCalledTimes(2);
   });
+
+  test('validateImagaCounts, given 0 dashboads, does not throw an error', () => {
+    const globMock = [
+      path.resolve(__dirname, '../mock_files/mock-quickstarts/mock-quickstart-6/config.yml'),
+    ];
+    validateImageCounts(globMock);
+    expect(core.setFailed).not.toHaveBeenCalled();
+    expect(global.console.warn).not.toHaveBeenCalled();
+  });
+
 });
