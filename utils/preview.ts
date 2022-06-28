99b5fe6116d11efdd56cb1d1b4f1216c537b2a6a
Zack Stickles <zstickles@newrelic.com>
2022-06-27 17:26:41 -0700

    wip: Update preview server to serve all files

--- utils/preview.ts
+++ utils/preview.ts
@@ -1,79 +1,72 @@
 import * as express from 'express';
-
-// Types
-import { Request, Response } from 'express';
-
-// Modules
 import * as path from 'path';
-import * as glob from 'glob';
 import * as cors from 'cors';
 
-// Helpers
-import { removeRepoPathPrefix } from './lib/helpers';
+import Quickstart from './lib/Quickstart';
 
-// App
+const GREEN = '\x1b[32m';
+const PORT = process.env.PORT || '3000';
+const BASE_PREVIEW_LINK =
+  'https://newrelic.com/instant-observability/preview?local=true';
+
 const app = express();
 
-const port: string = process.env.PORT || '3000';
-const QUICKSTART_DIRECTORY: string = process.argv[2];
-const PARENT_DIRECTORY: string = path.resolve(__dirname, '../quickstarts');
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+
+app.use('/', express.static(path.resolve(__dirname, '..')));
 
 /**
- * Async function to validate given path is to a single quickstart
- * @param parentDirectory - Quickstart home directory 
- * @param quickstartDirectory - Quickstart directory
-*/
-export const isPathValid = (parentDirectory: string, quickstartDirectory: string): boolean => {
-  const configFile = glob.sync(path.join(parentDirectory, `/${quickstartDirectory}/config.+(yml|yaml)`));
-  const fullPath = path.join(parentDirectory, `/${quickstartDirectory}`);
-
-  if (!configFile || configFile.length !== 1) {
-    console.error(`Could not find a config.yml or config.yaml at ${fullPath}.`);
-    return false;
+ * Helper function to ensure that an identifier has been supplied
+ * and that it is for a valid quickstart.
+ */
+const validateArgs = (identifier: string) => {
+  if (!identifier) {
+    console.error(
+      'You must provide a directory path to the quickstart. Example: yarn preview aws/amazon-athena'
+    );
+    process.exit(1);
   }
 
-  return true;
+  // FIXME: determine why this is using the _directory_ not the _configuration_
+  const quickstart = new Quickstart(
+    identifier,
+    path.resolve(__dirname, '..', 'quickstarts')
+  );
+
+  if (!quickstart.isValid) {
+    console.error(
+      `Could not find a config.yml or config.yaml for ${identifier}.`
+    );
+    process.exit(1);
+  }
 };
 
+/**
+ * Helper function to generate a preview link with an identifier
+ * and an optional port.
+ */
+const getPreviewLink = (identifier: string) => {
+  const port = PORT !== '&port=3000' ? PORT : '';
+  return `${BASE_PREVIEW_LINK}&quickstart=${identifier}${port}`;
+};
+
+/**
+ * Entrypoint.
+ */
 const main = async () => {
-    // Validate whether param was provided
-  if (!process.argv[2]) {
-    console.error("You must provide a directory path to the quickstart. Example: yarn preview aws/amazon-athena");
-    process.exit(1);
-  }
+  const identifier = process.argv[2];
 
-  /*
-        Set to localhost for dev purposes TODO: Set to instant observability later
-  */
-  let PREVIEW_LINK = 'https://newrelic.com/instant-observability/preview?local=true';
+  validateArgs(identifier);
 
-  if (port !== '3000') {
-    PREVIEW_LINK += `&port=${port}`;
-  }
+  const link = getPreviewLink(identifier);
 
-  const isValid = isPathValid(PARENT_DIRECTORY, QUICKSTART_DIRECTORY);
-
-  if (!isValid) {
-    process.exit(1);
-  }
-
-  app.use(cors({
-    origin: '*'
-  }));
-  app.use(`/quickstarts/${QUICKSTART_DIRECTORY}`, express.static(path.join(PARENT_DIRECTORY, `/${QUICKSTART_DIRECTORY}`)));
-
-  app.get('/', (req: Request, res: Response) => {
-    let response = glob.sync(path.join(PARENT_DIRECTORY, `/${QUICKSTART_DIRECTORY}/**/*.*`))
-                      
-    res.json(response.map(removeRepoPathPrefix));
-  });
-
-  app.listen(port, () => {
-
-    /*
-        Set to localhost for dev purposes TODO: Set to instant observability later
-    */
-    console.log("\x1b[32m", `Now serving your output on: http://localhost:${port} \n You can view your preview at: ${PREVIEW_LINK}`);
+  app.listen(PORT, () => {
+    console.log(`Now serving files on http://localhost:${PORT}`);
+    console.log(`${GREEN}You can view your preview at ${link}`);
   });
 };
 
@@ -82,6 +75,6 @@ const main = async () => {
  * This would be true if this was used in one of our GitHub workflows, but false when imported for use in a test.
  * See here: https://nodejs.org/docs/latest/api/modules.html#modules_accessing_the_main_module
  */
- if (require.main === module) {
+if (require.main === module) {
   main();
 }
