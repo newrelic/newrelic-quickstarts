20625c517be34e347bb37b73a3c524ecea995701
Mickey Ryan <mickeyryan@newrelic.com>
2022-06-28 13:52:03 -0400

    Add dashboard and alert files to GET response

--- utils/preview.ts
+++ utils/preview.ts
@@ -2,15 +2,20 @@ import * as express from 'express';
 import * as path from 'path';
 import * as cors from 'cors';
 import * as glob from 'glob';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
 
 // Types
 import { Request, Response } from 'express';
 
 import Quickstart from './lib/Quickstart';
+import { QuickstartConfig } from './types/QuickstartConfig';
 
 const GREEN = '\x1b[32m';
 const PORT = process.env.PORT || '3000';
 const PARENT_DIRECTORY = path.resolve(__dirname, '../quickstarts');
+const DASHBOARDS_DIRECTORY = path.resolve(__dirname, '../dashboards');
+const ALERTS_DIRECTORY = path.resolve(__dirname, '../alert-policies');
 const BASE_PREVIEW_LINK =
   'https://newrelic.com/instant-observability/preview?local=true';
 
@@ -20,16 +25,13 @@ const app = express();
  * Helper function to ensure that an identifier has been supplied
  * and that it is for a valid quickstart.
  */
-const validateArgs = (identifier: string) => {
+const validateArgs = (identifier: string, configFile: string) => {
   if (!identifier) {
     console.error(
       'You must provide a directory path to the quickstart. Example: yarn preview aws/amazon-athena'
     );
     process.exit(1);
   }
-
-  const configPath = glob.sync(path.join(PARENT_DIRECTORY, identifier, 'config.*'))[0] ?? '';
-  const configFile = configPath.split('/').pop();
   
   const quickstart = new Quickstart(
     `${identifier}/${configFile}`,
@@ -54,12 +56,24 @@ const getPreviewLink = (identifier: string) => {
 };
 
 /**
+ * Takes an absolute path and removes everything prior to and including 'newrelic-quickstarts' 
+ * @returns file paths starting at quickstarts/ | dashboards/ | alert-policies/ Example: dashboards/battlesnake-game-tracking/battlesnake-game-tracking.json
+ */
+const removePathPrefixes = (filePath: string) => filePath.split('/newrelic-quickstarts/')[1];
+
+/**
+ * Takes a string array of directories (dashboards for a quickstart) and base directory (dashboards | alert-policies) and returns all the files from the directories in a flat map 
+ */
+const getFilesFromDirectories = (directories: string[], rootDir: string) => directories.flatMap(dir => glob.sync(path.join(rootDir, `${dir}/*.*`)).map(removePathPrefixes))
+/**
  * Entrypoint.
  */
 const main = async () => {
   const identifier = process.argv[2];
+  const configPath = glob.sync(path.join(PARENT_DIRECTORY, identifier, 'config.*'))[0] ?? '';
+  const configFile = configPath.split('/').pop() ?? '';
 
-  validateArgs(identifier);
+  validateArgs(identifier, configFile);
 
   const link = getPreviewLink(identifier);
   
@@ -71,9 +85,17 @@ const main = async () => {
   app.use('/', express.static(path.resolve(__dirname, '..')));
 
   app.get('/', (req: Request, res: Response) => {
-    const response = glob.sync(path.join(PARENT_DIRECTORY, `/${identifier}/**/*.*`))
-                    .map(filepath => filepath.split('/newrelic-quickstarts/')[1]);                  
-    res.json(response);
+    const quickstartFiles = glob.sync(path.join(PARENT_DIRECTORY, `/${identifier}/**/*.*`))
+                    .map(removePathPrefixes);
+    
+    const config = yaml.load(
+      fs.readFileSync(configPath).toString('utf-8')
+    ) as QuickstartConfig;
+
+    const dashboardFiles = getFilesFromDirectories(config.dashboards || [], DASHBOARDS_DIRECTORY);
+    const alertPolicyFiles = getFilesFromDirectories(config.alertPolicies || [], ALERTS_DIRECTORY);
+    
+    res.json([...quickstartFiles, ...dashboardFiles, ...alertPolicyFiles]);
   });
 
   app.listen(PORT, () => {
