cdfe774ed065463f7493adee3338885174dcb4d7
Alec Swanson <aswanson@newrelic.com>
2022-07-28 15:50:10 -0700

    wip: test's other message

--- utils/dev.ts
+++ utils/dev.ts
@@ -1,6 +1,7 @@
 import Quickstart from './lib/Quickstart';
 
 const apache = new Quickstart('quickstarts/circleci/config.yml');
+// test
 
 (async () => {
   const mut = await apache.getMutationVariables(true);
