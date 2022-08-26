541e810d152b861bef5ebb83fed432d97d885fe9
Alec Swanson <aswanson@newrelic.com>
2022-08-26 13:07:57 -0700

    chore: remove deprecated scripting
    * Removes an old github action used to preview quickstarts

--- importer-config.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# How to find your accountId: https://docs.newrelic.com/docs/accounts/accounts-billing/account-setup/account-id/
-accountId=
-# How to create a User API Key: https://docs.newrelic.com/docs/apis/intro-apis/new-relic-api-keys/#user-key-create
-apiKey=
