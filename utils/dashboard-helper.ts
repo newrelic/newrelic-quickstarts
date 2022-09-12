8d469cab85f12ca23ba6760ffc96f2f292b42e89
Alec Swanson <aswanson@newrelic.com>
2022-09-12 10:51:06 -0700

    chore: add code to fetch dashboard files from PR

--- utils/dashboard-helper.ts
+++ utils/dashboard-helper.ts
@@ -1,4 +1,5 @@
-// At this point we are on the PR's ref, so we can operate on all dashboards
+import Dashboard from './lib/Dashboard';
+import { fetchPaginatedGHResults } from './lib/github-api-helpers';
 
 const guidRegex = /guid[`'"\) ]/;
 const entityGuidRegex = /entityGuid/;
@@ -6,10 +7,7 @@ const guidFieldRegex = /\"linkedEntityGuids\": (?:(?!null))/;
 const permissionsFieldRegex = /\"permissions\": /;
 const accountIdRegEx = /\"accountId\": (?:(?!0))/;
 
-import Dashboard from './lib/Dashboard';
-
 const checkLine = (line: string) => {
-
   const warningsFound = [];
 
   if (guidRegex.test(line)) {
@@ -29,20 +27,39 @@ const checkLine = (line: string) => {
   }
 
   return warningsFound;
-
 };
+
 const encodedNewline = '%0A';
 const createWarningComment = (warnings: string[]) => {
-  const commentMessage = [`### The PR checks have run and found the following warnings:${encodedNewline}`,];
+  const commentMessage = [
+    `### The PR checks have run and found the following warnings:${encodedNewline}`,
+  ];
   const tableHeader = `| Warning | Filepath | Line # | ${encodedNewline}| --- | --- | --- | `;
   commentMessage.push(tableHeader);
-  warnings.forEach(w => commentMessage.push(w));
+  warnings.forEach((w) => commentMessage.push(w));
   return commentMessage.join(encodedNewline);
 };
 
-const main = () => {
+const runHelper = async (prUrl?: string, token?: string): Promise<boolean> => {
+  if (!token) {
+    console.error(`Missing GITHUB_TOKEN environment variable`);
+    return false;
+  }
+
+  if (!prUrl) {
+    console.error(
+      `Missing arguments. Example: ts-node dashboard-helper.ts <pull request url>`
+    );
+    return false;
+  }
+
   const dashboards = Dashboard.getAll();
   const warnings: string[] = [];
+
+  const files = await fetchPaginatedGHResults(
+    new URL(`/files`, prUrl).href,
+    token
+  );
 
   for (const dash of dashboards) {
     const dashLines = JSON.stringify(dash.config, null, 2).split('\n');
@@ -50,16 +67,38 @@ const main = () => {
     dashLines.forEach((line, lineNumber) => {
       const output = checkLine(line);
       if (output.length > 0) {
-        output.forEach(o => warnings.push(`| ${o} | ${dash.configPath} | ${lineNumber + 1} |`));
+        output.forEach((o) =>
+          warnings.push(`| ${o} | ${dash.configPath} | ${lineNumber + 1} |`)
+        );
       }
     });
-  };
+  }
 
   if (warnings.length > 0) {
-    console.log("Found warnings:", warnings);
+    console.log('Found warnings:', warnings);
     const warningComment = createWarningComment(warnings);
     console.log(`::set-output name=comment::${warningComment}`);
   }
+
+  return true;
 };
 
-main();
+/**
+ * Gathers environment variables and arguments, then executes the script
+ */
+const main = async () => {
+  const isSuccess = await runHelper(process.argv[2], process.env.GITHUB_TOKEN);
+
+  if (!isSuccess) {
+    process.exit(1);
+  }
+};
+
+/**
+ * This allows us to check if the script was invoked directly from the command line, i.e 'ts-node dashboard-helper.ts', or if it was imported.
+ * This would be true if this was used in one of our GitHub workflows, but false when imported for use in a test.
+ * See here: https://nodejs.org/docs/latest/api/modules.html#modules_accessing_the_main_module
+ */
+if (require.main === module) {
+  main();
+}
