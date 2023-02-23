1c82def1233ae61c617f17ec49c497dd74d332ec
Joe Gregory <jgregory@newrelic.com>
2023-02-23 09:55:24 -0800

    chore: Uncomment spyOn lines

--- utils/lib/__tests__/DataSource.test.js
+++ utils/lib/__tests__/DataSource.test.js
@@ -16,8 +16,8 @@ jest.mock('fs', () => {
   };
 });
 
-// jest.spyOn(global.console, 'log').mockImplementation(() => {});
-// jest.spyOn(global.console, 'error').mockImplementation(() => {});
+jest.spyOn(global.console, 'log').mockImplementation(() => {});
+jest.spyOn(global.console, 'error').mockImplementation(() => {});
 
 const MOCK_FILES_BASEPATH = path.resolve(__dirname, '../../mock_files');
 
