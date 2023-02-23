69bb1ffa99c64f63d52df0723c7f47b43ec5d298
mlosier <mlosier@newrelic.com>
2023-02-23 11:54:46 -0800

    refactor: Remove published datasource fetching

--- utils/migration_scripts/assign-data-sources-to-quickstarts.ts
+++ utils/migration_scripts/assign-data-sources-to-quickstarts.ts
@@ -3,109 +3,7 @@ import * as yaml from 'js-yaml';
 import * as path from 'path';
 
 import Quickstart from '../lib/Quickstart';
-import DataSource, { getAllDataSourceFiles } from '../lib/DataSource';
-import { fetchNRGraphqlResults } from '../lib/nr-graphql-helpers';
 import { passedProcessArguments } from '../lib/helpers';
-
-const gql = String.raw;
-
-export const PUBLISHED_DATA_SOURCES_QUERY = gql`
-  {
-    actor {
-      nr1Catalog {
-        search(filter: { types: DATA_SOURCE }) {
-          results {
-            ... on Nr1CatalogDataSource {
-              id
-              metadata {
-                displayName
-                install {
-                  fallback {
-                    __typename
-                    ... on Nr1CatalogLinkInstallDirective {
-                      url
-                    }
-                    ... on Nr1CatalogNerdletInstallDirective {
-                      nerdletId
-                      nerdletState
-                    }
-                  }
-                  primary {
-                    __typename
-                    ... on Nr1CatalogLinkInstallDirective {
-                      url
-                    }
-                    ... on Nr1CatalogNerdletInstallDirective {
-                      nerdletId
-                      nerdletState
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`;
-
-enum InstallDirectiveTypename {
-  Link = 'Nr1CatalogLinkInstallDirective',
-  Nerdlet = 'Nr1CatalogNerdletInstallDirective',
-}
-
-interface CatalogDataSourceLinkInstall {
-  __typename: InstallDirectiveTypename.Link;
-  url: string;
-}
-
-interface CatalogDataSourceNerdletInstall {
-  __typename: InstallDirectiveTypename.Nerdlet;
-  nerdletId: string;
-  nerdletState: Record<string, unknown> | null;
-}
-
-type CatalogDataSourceInstall =
-  | CatalogDataSourceNerdletInstall
-  | CatalogDataSourceLinkInstall;
-
-interface CatalogDataSource {
-  id: string;
-  metadata: {
-    displayName: string;
-    install: {
-      primary: CatalogDataSourceInstall;
-      fallback: CatalogDataSourceInstall | null;
-    };
-  };
-}
-
-interface PublishedDataSourceSearchResults {
-  actor: {
-    nr1Catalog: {
-      search: {
-        results: CatalogDataSource[];
-      };
-    };
-  };
-}
-
-const getAllPublishedDataSources = async () => {
-  const { data, errors } = await fetchNRGraphqlResults<
-    {},
-    PublishedDataSourceSearchResults
-  >({
-    queryString: PUBLISHED_DATA_SOURCES_QUERY,
-    variables: {},
-  });
-
-  const {
-    search: { results },
-  } = data.actor.nr1Catalog;
-
-  return { results, errors };
-};
 
 const parseCSV = (csvString: string) => {
   const csvRows = csvString.split('\r\n');
@@ -127,7 +25,6 @@ const NOT_DATASOURCE_IDS = ['TO_BE_CREATED', 'MANUAL'];
 const main = async () => {
   const [csvRelativePath] = passedProcessArguments();
   const quickstarts = Quickstart.getAll();
-  //const publishedDataSources = await getAllPublishedDataSources(); // Switch to own full data source query
 
   const fullCSVPath = path.resolve(__dirname, csvRelativePath);
 
