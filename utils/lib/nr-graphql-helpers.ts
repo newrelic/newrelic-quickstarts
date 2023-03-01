6f314a2a0e422b5d066bd56b5d07955fd20cb789
Alec Swanson <aswanson@newrelic.com>
2023-03-01 11:52:00 -0800

    fix: Stop hiding errors
    * fixes some code that was dropping errors

--- utils/lib/nr-graphql-helpers.ts
+++ utils/lib/nr-graphql-helpers.ts
@@ -114,6 +114,10 @@ export const translateMutationErrors = (
     } else {
       console.error(`- ${error.message}`);
     }
+
+    if (error.message) {
+      console.error(error.message);
+    }
   });
 
   if (componentErrors.length > 0) {
