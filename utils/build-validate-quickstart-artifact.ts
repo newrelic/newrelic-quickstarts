68085ffa3ec142df7267f0727038ab49205be8f1
Andrew Anguiano <aanguiano@newrelic.com>
2024-08-29 17:10:01 -0400

    WIP: Cleanup

--- utils/build-validate-quickstart-artifact.ts
+++ utils/build-validate-quickstart-artifact.ts
@@ -1,6 +1,3 @@
-import fs from 'fs';
-import path from 'path';
-
 import Quickstart from "./lib/Quickstart";
 import DataSource from "./lib/DataSource";
 import Alert from "./lib/Alert";
@@ -8,14 +5,15 @@ import Dashboard from "./lib/Dashboard";
 import Ajv from 'ajv';
 
 const main = () => {
+  const schema = require('./schema/artifact.json');
+
   // 1. Fetch all quickstarts, datasources, alerts, and dashboards
   const quickstarts = Quickstart.getAll().map((quickstart) => quickstart.config);
   //const datasources = DataSource.getAll().map((datasource) => datasource.config);
   const alerts = Alert.getAll().map((alert) => alert.config);
   const dashboards = Dashboard.getAll().map((dashboard) => dashboard.config);
 
-  //console.log(datasources[0]);
-  const coreDatasourceIds = JSON.parse(fs.readFileSync(path.join(__dirname, 'schema', 'core-datasource-ids.json'), 'utf8'));
+  const coreDatasourceIds = require('./schema/core-datasource-ids.json');
   //const communityDatasourceIds = datasources.map((ds) => { console.log(ds); return ds.id });
 
   // TODO: consider json-schema-to-ts package to infer type from JSON schema
@@ -30,7 +28,7 @@ const main = () => {
 
   // 3. Validate the artifact
   const ajv = new Ajv();
-  ajv.validate(require('./schema/artifact.json'), artifact);
+  ajv.validate(schema, artifact);
 
   console.log(ajv.errors, 'ERRORS');
 }
